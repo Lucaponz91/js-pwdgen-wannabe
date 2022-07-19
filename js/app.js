1402f59f43171e086e21008a3dd3a759dffaef75
Lucaponz91 <lucapo1991@msn.com>
2022-07-19 16:34:14 +0200

    psw add

--- js/app.js
+++ js/app.js
@@ -1,21 +1,25 @@
 // Chiedi all’utente il suo nome,
 
 let nomeUtente = prompt('Qual è il tuo nome?');
-console.log('Ti chiami ' + nomeUtente );
-console.dir(nomeUtente);
+// console.log('Ti chiami ' + nomeUtente );
+// console.dir(nomeUtente);
 // const nome = document.getElementById('nome');
+
 
 
 
 // poi chiedi il suo cognome,
 
-// let cognomeUtente = prompt('Qual è il tuo cognome?');
+let cognomeUtente = prompt('Qual è il tuo cognome?');
 // const cognome = 
 
 // poi chiedi il suo colore preferito
 
-// let colorePreferito = prompt('Qual è il tuo colore preferito?')
+let colorePreferito = prompt('Qual è il tuo colore preferito?')
 
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
-// const numero = 21
+let psw = nomeUtente + cognomeUtente + colorePreferito + 21
+console.log(psw)
+console.dir(password)
+password.innerHTML('psw')
