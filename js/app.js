31effb47bd09edea3fe22fceccd7aa2184255373
Lucaponz91 <lucapo1991@msn.com>
2022-07-19 16:19:22 +0200

    wip nome

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// Chiedi all’utente il suo nome,
+
+let nomeUtente = prompt('Qual è il tuo nome?');
+console.log('Ti chiami ' + nomeUtente );
+console.dir(nomeUtente);
+// const nome = document.getElementById('nome');
+
+
+
+// poi chiedi il suo cognome,
+
+// let cognomeUtente = prompt('Qual è il tuo cognome?');
+// const cognome = 
+
+// poi chiedi il suo colore preferito
+
+// let colorePreferito = prompt('Qual è il tuo colore preferito?')
+
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+// const numero = 21
