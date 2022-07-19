a60fce958e6d2025e5a4c409f9a3857c278e7eb2
Lucaponz91 <lucapo1991@msn.com>
2022-07-19 16:54:15 +0200

    add dati inseriti html

--- Bonus/js/app.js
+++ Bonus/js/app.js
@@ -1,7 +1,7 @@
 // Chiedi all’utente il suo nome,
 
 let nomeUtente = prompt('Qual è il tuo nome?');
-nome.innerHTML = nomeUtente
+nome.innerHTML = 'Ti chiami ' + nomeUtente
 // console.log('Ti chiami ' + nomeUtente );
 // console.dir(nomeUtente);
 // const nome = document.getElementById('nome');
@@ -12,15 +12,17 @@ nome.innerHTML = nomeUtente
 // poi chiedi il suo cognome,
 
 let cognomeUtente = prompt('Qual è il tuo cognome?');
+cognome.innerHTML = 'Il tuo cognome è ' + cognomeUtente;
 // const cognome = 
 
 // poi chiedi il suo colore preferito
 
-let colorePreferito = prompt('Qual è il tuo colore preferito?')
+let colorePreferito = prompt('Qual è il tuo colore preferito?');
+colore.innerHTML = 'Il tuo colore preferito è il ' + colorePreferito;
 
 // Infine scrivi sulla pagina nomecognomecolorepreferitoNumeroRandom
 
 let psw = nomeUtente + cognomeUtente + colorePreferito + (Math.floor(Math.random() * 100) + 1);
-console.log(psw)
-console.dir(password)
-password.innerHTML = psw
+console.log(psw);
+console.dir(password);
+password.innerHTML = psw;
